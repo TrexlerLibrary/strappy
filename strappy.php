1ceeef83ccbd87c48a9182f999133bdfcab24c6f
Adam Malantonio <adam.malantonio@gmail.com>
2013-10-28 00:16:37 -0400

    modified landing page js

--- strappy.php
+++ strappy.php
@@ -8,6 +8,8 @@
 <!doctype html>
 <html lang="en">
 <head>
+<!-- Begin yr ILL strap template here -->
+
 <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.no-icons.min.css" rel="stylesheet">
 <script type="text/javascript" src="code39.js"></script>
 
@@ -29,57 +31,53 @@
 </style>
 <body>
   <div id="container" class="span4">
-  <div id="otherthing">
-    <h4>Patron: Please do not remove this strap</h4>
-    <p>Lender Symbol: <strong><?=$lender?></strong></p>
-    <p>Patron: <strong><?=$patron?></strong></p>
-    <p>Barcode/ILL#</p>
-    <p id="inputdata">(<?=$barcode?>)</p>
-    <p>Due Date: <h3 contenteditable="true"><?=$dueDate?></h3></p>
-    <p><strong>OVERDUE FINE OF $2.00 PER DAY</strong> </p>
+    <div id="otherthing">
+      <h4>Patron: Please do not remove this strap</h4>
+      <p>Lender Symbol: <strong><?=$lender?></strong></p>
+      <p>Patron: <strong><?=$patron?></strong></p>
+      <p>Barcode/ILL#</p>
+      <p id="barcode">(<?=$barcode?>)</p>
+      <p>Due Date: <h3 class="dueDate" contenteditable="true"><?=$dueDate?></h3></p>
+      <p><strong>OVERDUE FINE OF $2.00 PER DAY</strong> </p>
 
-    <p>Requests for renewal should be made on, or before due date. <p>
-    <p>To request a renewal, please contact the ILL office by email at <srong>illd@muhlenberg.edu</strong> or by phone at <strong>484-664-3510</strong>.
-       </br>Please include your name, the ILL/Barcode number, and book title.</p>
+      <p>Requests for renewal should be made on, or before due date. <p>
+      <p>To request a renewal, please contact the ILL office by email at <srong>illd@muhlenberg.edu</strong> or by phone at <strong>484-664-3510</strong>.
+         </br>Please include your name, the ILL/Barcode number, and book title.</p>
 
-	<p><b>Please Do Not Shelve</b></p>
+  	<p><b>Please Do Not Shelve</b></p>
 
-	<h4>Return to Lending Services Desk</h4>
+  	<h4>Return to Lending Services Desk</h4>
 
-	<h4>Patron: Please do not remove this strap</h4>
+  	<h4>Patron: Please do not remove this strap</h4>
 
-	<h5>Interlibrary loan books are subject to recall by the lending library at any time.</h5>
+  	<h5>Interlibrary loan books are subject to recall by the lending library at any time.</h5>
+    </div>
   </div>
 
+<!-- End yr ILL strap template here -->
+
 <script type="text/javascript">
-  function get_object(id) {
-   var object = null;
+  var barcode = document.getElementById('barcode')
+    , dueDate = document.querySelector('.dueDate');
 
-   if (document.layers) {
-    object = document.layers[id];
+  barcode.innerHTML = DrawCode39Barcode(barcode.innerHTML, 0);
+  var spans = document.querySelectorAll('#barcode div span');
+  
+  /*
+   *  because we can, let's escape the asterisks that appear b/c of code39.js
+   */
+  spans[(spans.length - 1)].innerText = spans[(spans.length - 1)].innerText.replace(/\*/g, '')
+  
+  /* 
+   * if no due date is provided from worldcat, let's draw the user's attention to it
+   *   and have them fill it in
+   */
 
-   } else if (document.all) {
-    object = document.all[id];
-
-   } else if (document.getElementById) {
-    object = document.getElementById(id);
-   }
-   return object;
+  if ( dueDate.innerHTML === "" ) {
+    alert("Hey I need a due date!");
+    dueDate.focus();
   }
-get_object("inputdata").innerHTML=DrawCode39Barcode(get_object("inputdata").innerHTML,0);
 </script>
 
 </body>
-
-<!--  shooby taylor has a posse  -->
-
-
-<!--  BELOW FIND THE BOOKMARKLET STUFF TO COPY AND PASTE -->
-
-<!--
-javascript:(function () {var d=document.getElementsByClassName("yui-field-dueDate"),l=document.getElementsByClassName("lendingInformationExtra"),p=document.getElementsByClassName("patronExtra"),b=document.getElementsByClassName("accordionRequestDetailsRequestId"),url,path="http://malantonio.com/strappy.php";if(!d|!d.length|!l|!l.length|!p|!p.length|!b|!b.length){return false;}d=d[d.length-1].innerHTML;l=l[l.length-1].innerHTML.replace("(Supplier: ", "").replace(")", "");p=p[p.length-1].innerHTML.replace("(", "").replace(")", "");b=b[b.length-1].innerHTML;window.open(path+"?barcode="+b+"&lender="+l+"&patron="+encodeURI(p)+"&dueDate="+encodeURI(d),"_blank");})()
--->
-
-<!-- END OF BOOKMARKLET STUFF -->
-
 </html>
